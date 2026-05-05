b13626a152e917647be4f884290badd341f95621
sammietwayne <sammietwyne@gmail.com>
2026-05-05 21:56:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,7 @@ const navLinks = document.getElementById("navLinks");
 if (menuBtn && navLinks) {
   menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("open");
-
-    if (navLinks.classList.contains("open")) {
-      menuBtn.textContent = "×";
-    } else {
-      menuBtn.textContent = "☰";
-    }
+    menuBtn.textContent = navLinks.classList.contains("open") ? "×" : "☰";
   });
 
   navLinks.querySelectorAll("a").forEach((link) => {
